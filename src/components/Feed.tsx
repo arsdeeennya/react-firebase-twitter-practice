37e0e8edcbdf1d4588bee8525ad99ff4365cc423
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-08-30 19:27:17 +0900

    ぐーぐるろぐいん

---
+++ src/components/Feed.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Feed.module.css";
+
+const Feed: React.FC = () => {
+
+  return (
+    <div className={styles.feed}>
+      
+    </div>
+  );
+};
+
+export default Feed;
